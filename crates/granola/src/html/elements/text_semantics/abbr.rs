733519855e97135eceb69f8c768df68202b223ca
lpnh <paniguel.lpnh@gmail.com>
2026-04-18 16:25:41 -0300

    add abbr element

---
+++ crates/granola/src/html/elements/text_semantics/abbr.rs
@@ -0,0 +1,89 @@
+use askama::Template;
+use std::{
+    borrow::Cow,
+    fmt::{Debug, Display},
+    marker::PhantomData,
+};
+
+use crate::{filters, prelude::*};
+
+pub trait AbbrTag: Default + Clone + Debug + 'static {
+    const CLASS: Option<&'static str> = None;
+    /// Permitted ARIA roles: any
+    const ROLE: Option<&'static str> = None;
+    type Content: Display + Default + Clone + Debug = Cow<'static, str>;
+}
+
+impl AbbrTag for () {}
+
+/// The HTML `<abbr>` element.
+///
+/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/abbr)
+///
+/// # Example
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let abbr: HtmlAbbr = HtmlAbbr::empty().id("abbreviation");
+///
+/// assert_eq!(abbr.bake(),
+/// r#"<abbr id="abbreviation"></abbr>"#);
+/// ```
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let abbr: HtmlAbbr = HtmlAbbr::new("TMNT").title("Teenage Mutant Ninja Turtles");
+///
+/// assert_eq!(abbr.bake(),
+/// r#"<abbr title="Teenage Mutant Ninja Turtles">TMNT</abbr>"#);
+/// ```
+///
+/// # Askama template
+///
+/// ```askama
+/// <abbr
+///   {{- global_attrs -}}
+///   {{- data_attrs -}}
+///   {{- event_handlers -}}
+///   {{- global_aria_attrs -}}
+/// >{{ content | kirei(2, 70) }}</abbr>
+/// ```
+#[derive(Debug, Clone, PartialEq, Default, Template, Granola, MutAttrs)]
+#[template(ext = "html", in_doc = true, escape = "none")]
+pub struct HtmlAbbr<M: AbbrTag = ()> {
+    _marker: PhantomData<M>,
+    pub content: M::Content,
+    pub global_attrs: GlobalAttrs,
+    pub data_attrs: DataAttrs,
+    pub event_handlers: EventHandlers,
+    pub global_aria_attrs: GlobalAriaAttrs,
+}
+
+impl<M: AbbrTag> HtmlAbbr<M> {
+    pub fn new(content: impl Into<M::Content>) -> Self {
+        let mut s = Self {
+            content: content.into(),
+            ..Default::default()
+        };
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+
+    pub fn empty() -> Self {
+        let mut s = Self::default();
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+}
