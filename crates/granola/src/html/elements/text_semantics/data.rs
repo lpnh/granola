01b122aca7a101cb867405195b5f0aada72e6a87
lpnh <paniguel.lpnh@gmail.com>
2026-04-19 16:26:42 -0300

    add data element

---
+++ crates/granola/src/html/elements/text_semantics/data.rs
@@ -0,0 +1,93 @@
+use askama::Template;
+use std::{
+    borrow::Cow,
+    fmt::{Debug, Display},
+    marker::PhantomData,
+};
+
+use crate::{filters, prelude::*};
+
+pub trait DataTag: Default + Clone + Debug + 'static {
+    const CLASS: Option<&'static str> = None;
+    /// Permitted ARIA roles: any
+    const ROLE: Option<&'static str> = None;
+    type Content: Display + Default + Clone + Debug = Cow<'static, str>;
+}
+
+impl DataTag for () {}
+
+/// The HTML `<data>` element.
+///
+/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/data)
+///
+/// # Example
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let data: HtmlData = HtmlData::empty().id("data");
+///
+/// assert_eq!(data.bake(),
+/// r#"<data id="data"></data>"#);
+/// ```
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let data: HtmlData = HtmlData::new("$13.37").value("1337");
+///
+/// assert_eq!(data.bake(),
+/// r#"<data value="1337">$13.37</data>"#);
+/// ```
+///
+/// # Askama template
+///
+/// ```askama
+/// <data
+///   {{- global_attrs -}}
+///   {{- specific_attrs -}}
+///   {{- data_attrs -}}
+///   {{- event_handlers -}}
+///   {{- global_aria_attrs -}}
+/// >{{ content | kirei(2, 70) }}</data>
+/// ```
+#[derive(Debug, Clone, PartialEq, Default, Template, Granola, MutAttrs)]
+#[template(ext = "html", in_doc = true, escape = "none")]
+pub struct HtmlData<M: DataTag = ()> {
+    _marker: PhantomData<M>,
+    pub content: M::Content,
+    pub global_attrs: GlobalAttrs,
+    pub specific_attrs: SpecificAttrs,
+    pub data_attrs: DataAttrs,
+    pub event_handlers: EventHandlers,
+    pub global_aria_attrs: GlobalAriaAttrs,
+}
+
+impl<M: DataTag> HtmlData<M> {
+    pub fn new(content: impl Into<M::Content>) -> Self {
+        let mut s = Self {
+            content: content.into(),
+            ..Default::default()
+        };
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        s
+    }
+
+    pub fn empty() -> Self {
+        let mut s = Self::default();
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        s
+    }
+
+    /// Machine-readable value.
+    ///
+    /// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/data#value)
+    pub fn value(mut self, value: impl Into<Cow<'static, str>>) -> Self {
+        self.specific_attrs = self.specific_attrs.add_attr("value", value.into());
+        self
+    }
+}
