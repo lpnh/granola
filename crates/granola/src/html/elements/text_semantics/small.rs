37b42820bd9cdfa3cdc2d61f9c306ea9c1a066f9
lpnh <paniguel.lpnh@gmail.com>
2026-04-20 18:33:46 -0300

    add small element

---
+++ crates/granola/src/html/elements/text_semantics/small.rs
@@ -0,0 +1,91 @@
+use askama::Template;
+use std::{
+    borrow::Cow,
+    fmt::{Debug, Display},
+    marker::PhantomData,
+};
+
+use crate::{filters, prelude::*};
+
+pub trait SmallTag: Default + Clone + Debug + 'static {
+    const CLASS: Option<&'static str> = None;
+    /// Permitted ARIA roles: any
+    const ROLE: Option<&'static str> = None;
+    type Content: Display + Default + Clone + Debug = Cow<'static, str>;
+}
+
+impl SmallTag for () {}
+
+/// The HTML `<small>` element.
+///
+/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/small)
+///
+/// # Example
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let small: HtmlSmall = HtmlSmall::empty().id("side_comment");
+///
+/// assert_eq!(small.bake(),
+/// r#"<small id="side_comment"></small>"#);
+/// ```
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let unlicense = "This is free and unencumbered software released into the public domain.";
+///
+/// let small: HtmlSmall = HtmlSmall::new(unlicense);
+///
+/// assert_eq!(small.bake(),
+/// r#"<small>This is free and unencumbered software released into the public domain.</small>"#);
+/// ```
+///
+/// # Askama template
+///
+/// ```askama
+/// <small
+///   {{- global_attrs -}}
+///   {{- data_attrs -}}
+///   {{- event_handlers -}}
+///   {{- global_aria_attrs -}}
+/// >{{ content | kirei(2) }}</small>
+/// ```
+#[derive(Debug, Clone, PartialEq, Default, Template, Granola, MutAttrs)]
+#[template(ext = "html", in_doc = true, escape = "none")]
+pub struct HtmlSmall<M: SmallTag = ()> {
+    _marker: PhantomData<M>,
+    pub content: M::Content,
+    pub global_attrs: GlobalAttrs,
+    pub data_attrs: DataAttrs,
+    pub event_handlers: EventHandlers,
+    pub global_aria_attrs: GlobalAriaAttrs,
+}
+
+impl<M: SmallTag> HtmlSmall<M> {
+    pub fn new(content: impl Into<M::Content>) -> Self {
+        let mut s = Self {
+            content: content.into(),
+            ..Default::default()
+        };
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+
+    pub fn empty() -> Self {
+        let mut s = Self::default();
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+}
