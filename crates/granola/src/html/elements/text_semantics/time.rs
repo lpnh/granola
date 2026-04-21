51680381614f81efb9c937b372d051bba5b0fce6
lpnh <paniguel.lpnh@gmail.com>
2026-04-21 15:02:42 -0300

    add time element

---
+++ crates/granola/src/html/elements/text_semantics/time.rs
@@ -0,0 +1,99 @@
+use askama::Template;
+use std::{
+    borrow::Cow,
+    fmt::{Debug, Display},
+    marker::PhantomData,
+};
+
+use crate::{filters, prelude::*};
+
+pub trait TimeTag: Default + Clone + Debug + 'static {
+    const CLASS: Option<&'static str> = None;
+    /// Permitted ARIA roles: any
+    const ROLE: Option<&'static str> = None;
+    type Content: Display + Default + Clone + Debug = Cow<'static, str>;
+}
+
+impl TimeTag for () {}
+
+/// The HTML `<time>` element.
+///
+/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/time)
+///
+/// # Example
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let time: HtmlTime = HtmlTime::empty().id("datetime");
+///
+/// assert_eq!(time.bake(),
+/// r#"<time id="datetime"></time>"#);
+/// ```
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let time: HtmlTime = HtmlTime::new("Unix epoch").datetime("1970-01-01T00:00:00Z");
+///
+/// assert_eq!(time.bake(),
+/// r#"<time datetime="1970-01-01T00:00:00Z">Unix epoch</time>"#);
+/// ```
+///
+/// # Askama template
+///
+/// ```askama
+/// <time
+///   {{- global_attrs -}}
+///   {{- specific_attrs -}}
+///   {{- data_attrs -}}
+///   {{- event_handlers -}}
+///   {{- global_aria_attrs -}}
+/// >{{ content | kirei(2) }}</time>
+/// ```
+#[derive(Debug, Clone, PartialEq, Default, Template, Granola, MutAttrs)]
+#[template(ext = "html", in_doc = true, escape = "none")]
+pub struct HtmlTime<M: TimeTag = ()> {
+    _marker: PhantomData<M>,
+    pub content: M::Content,
+    pub global_attrs: GlobalAttrs,
+    pub specific_attrs: SpecificAttrs,
+    pub data_attrs: DataAttrs,
+    pub event_handlers: EventHandlers,
+    pub global_aria_attrs: GlobalAriaAttrs,
+}
+
+impl<M: TimeTag> HtmlTime<M> {
+    pub fn new(content: impl Into<M::Content>) -> Self {
+        let mut s = Self {
+            content: content.into(),
+            ..Default::default()
+        };
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+
+    pub fn empty() -> Self {
+        let mut s = Self::default();
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+
+    /// Machine-readable datetime representation.
+    ///
+    /// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time#datetime)
+    pub fn datetime(mut self, value: impl Into<Cow<'static, str>>) -> Self {
+        self.specific_attrs = self.specific_attrs.add_attr("datetime", value.into());
+        self
+    }
+}
