fc9b12e568b8ea8cec549feb9559cba8ea87f479
lpnh <paniguel.lpnh@gmail.com>
2026-04-24 17:30:09 -0300

    add aside element

---
+++ crates/granola/src/html/elements/sections/aside.rs
@@ -0,0 +1,94 @@
+use askama::Template;
+use std::{
+    borrow::Cow,
+    fmt::{Debug, Display},
+    marker::PhantomData,
+};
+
+use crate::{filters, prelude::*};
+
+pub trait AsideTag: Default + Clone + Debug + 'static {
+    const CLASS: Option<&'static str> = None;
+    /// Permitted ARIA roles: feed, none, note, presentation, region, search
+    const ROLE: Option<&'static str> = None;
+    type Content: Display + Default + Clone + Debug = Cow<'static, str>;
+}
+
+impl AsideTag for () {}
+
+/// The HTML `<aside>` element.
+///
+/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/aside)
+///
+/// # Example
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let aside: HtmlAside = HtmlAside::empty().id("aside");
+///
+/// assert_eq!(aside.bake(),
+/// r#"<aside id="aside"></aside>"#);
+/// ```
+///
+/// ```rust
+/// use granola::prelude::*;
+///
+/// let tip: HtmlStrong = HtmlStrong::new("Tip:");
+/// let content: HtmlP = HtmlP::new(bake_inline![tip, " trust your senses more than the timer."]);
+///
+/// let aside: HtmlAside = HtmlAside::new(bake_newline!(content)).role("note");
+///
+/// assert_eq!(aside.bake(),
+/// r#"<aside role="note">
+///   <p><strong>Tip:</strong> trust your senses more than the timer.</p>
+/// </aside>"#);
+/// ```
+///
+/// # Askama template
+///
+/// ```askama
+/// <aside
+///   {{- global_attrs -}}
+///   {{- data_attrs -}}
+///   {{- event_handlers -}}
+///   {{- global_aria_attrs -}}
+/// >{{ content | kirei(2) }}</aside>
+/// ```
+#[derive(Debug, Clone, PartialEq, Default, Template, Granola, MutAttrs)]
+#[template(ext = "html", in_doc = true, escape = "none")]
+pub struct HtmlAside<M: AsideTag = ()> {
+    _marker: PhantomData<M>,
+    pub content: M::Content,
+    pub global_attrs: GlobalAttrs,
+    pub data_attrs: DataAttrs,
+    pub event_handlers: EventHandlers,
+    pub global_aria_attrs: GlobalAriaAttrs,
+}
+
+impl<M: AsideTag> HtmlAside<M> {
+    pub fn new(content: impl Into<M::Content>) -> Self {
+        let mut s = Self {
+            content: content.into(),
+            ..Default::default()
+        };
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+
+    pub fn empty() -> Self {
+        let mut s = Self::default();
+        if let Some(class) = M::CLASS {
+            s = s.class(class);
+        }
+        if let Some(role) = M::ROLE {
+            s = s.role(role);
+        }
+        s
+    }
+}
