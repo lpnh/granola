b4a635a2397b5d211877791b4ba79490b2657047
lpnh <paniguel.lpnh@gmail.com>
2026-08-21 08:15:11 -0300

    fix css content property

--- crates/granola/src/recipes/css/properties/content.rs
+++ crates/granola/src/recipes/css/properties/content.rs
@@ -1,23 +1,23 @@
 use crate::prelude::*;
 
-/// The recipe for the CSS `value` property.
+/// The recipe for the CSS `content` property.
 ///
-/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/value)
+/// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/content)
 ///
 /// # Example
 ///
 /// ```rust
 /// use granola::{prelude::*, recipes::*};
 ///
-/// let css_value = CssDeclaration::from(Content).value(r#""this is new""#);
+/// let css_content = CssDeclaration::from(Content).value(r#""this is new""#);
 ///
-/// assert_eq!(css_value.bake(), r#"value: "this is new";"#);
+/// assert_eq!(css_content.bake(), r#"content: "this is new";"#);
 /// ```
 #[derive(Default, Debug, Clone, PartialEq)]
 pub struct Content;
 
 impl DeclarationRecipe for Content {
     fn property_recipe() -> Bake {
-        "value".into()
+        "content".into()
     }
 }
